2b969c5de50614697cbe504607558a4b6495522c
MrKarne <maneeshwarkarne@gmail.com>
2025-10-31 22:38:52 +0530

    Done

--- .eleventy.js
+++ .eleventy.js
@@ -9,7 +9,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/css')
     eleventyConfig.addPassthroughCopy('./src/fonts')
     eleventyConfig.addPassthroughCopy('./src/robots.txt')
-    eleventyConfig.addPassthroughCopy('./src/sitemap.xml')
+    // eleventyConfig.addPassthroughCopy('./src/sitemap.xml')
     eleventyConfig.addPassthroughCopy('./src/admin/config.yml')
     
     // --- Filters ---
