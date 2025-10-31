c41dcea0497b85ecbfd6f157dafb2fb96f6fa0dd
MrKarne <maneeshwarkarne@gmail.com>
2025-10-31 17:32:40 +0530

    addpassthorugh config

--- .eleventy.js
+++ .eleventy.js
@@ -1,48 +1,3 @@
-// const { DateTime } = require('luxon')
-
-// module.exports = function(eleventyConfig) {
-
-//     // tell to get it css . if eleventy doesnt pass thruough something from src to public. you gotta tell it explicitly to do so.
-//     // eleventyConfig.addPassthroughCopy('./src/style.css')
-//     // eleventyConfig.addPassthroughCopy('./src/assets')
-    
-//     eleventyConfig.addPassthroughCopy('./src/images')
-//     eleventyConfig.addPassthroughCopy('./src/js')
-//     eleventyConfig.addPassthroughCopy('./src/css')
-//     eleventyConfig.addPassthroughCopy('./src/fonts')
-//     // eleventyConfig.addPassthroughCopy('./src/blog/blogs.json')
-//     // eleventyConfig.addPassthroughCopy('./src/admin/config.yml')
-//     eleventyConfig.addPassthroughCopy('./src/robots.txt')
-//     eleventyConfig.addPassthroughCopy('./src/sitemap.xml')
-
-//     // eleventyConfig.addPassthroughCopy('./src/admin/config.yml')
-    
-//     // date filter
-//     eleventyConfig.addFilter("postDate", (dateObj) => {
-//         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
-//     })
-
-//     // blog collection
-//     eleventyConfig.addCollection("post", function(collectionApi) {
-//         return collectionApi.getFilteredByGlob("./src/blog/*.md").sort((a, b) => {
-//             return b.date - a.date; // Sort by descending date
-//         });
-//     });
-
-//     return {
-//         dir: {
-//             input: "src",
-//             output: "public"
-//         },
-//         pathPrefix: "/",
-//         htmlTemplateEngine: "njk",
-//         markdownTemplateEngine: "njk",
-//         dataTemplateEngine: "njk",
-//         templateFormats: ["html", "njk", "md"],
-//     }
-
-// }
-
 const { DateTime } = require('luxon')
 const slugify = require('slugify') // 👈 ADD THIS LINE
 
@@ -55,6 +10,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/fonts')
     eleventyConfig.addPassthroughCopy('./src/robots.txt')
     eleventyConfig.addPassthroughCopy('./src/sitemap.xml')
+    eleventyConfig.addPassthroughCopy('./src/admin/config.yml')
     
     // --- Filters ---
     // Standard date filter (using your existing name)
